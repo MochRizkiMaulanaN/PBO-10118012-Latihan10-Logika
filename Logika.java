22c2d77dbd7511dcd1e2cd00fc8a3581f387edad
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2019-09-26 15:03:42 +0700

    Add files via upload

---
+++ Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+/**
+ * Nama     : Moch Rizki Maulana N
+ * Kelas    : IF-1 2018
+ * Nim      : 10118012
+ * Deskripsi Program : Logika
+ *
+ * @author LENOVO
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur= 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status : "+dewasa);
+    }
+    
+}
